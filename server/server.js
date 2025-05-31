26362782e812e6e76ec3a9c62a84f3353b772d40
Rajgupta764 <smartsonuraj76458@gmail.com>
2025-05-31 19:27:24 +0530

    port changed

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ import noteRoutes from "./routes/notes.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 
 // Middlewares
 app.use(cors());
